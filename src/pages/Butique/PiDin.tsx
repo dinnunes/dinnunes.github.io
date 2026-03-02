404f326796d53d2216e0cb85e986060c583ef67f
Din <dinnuness@gmail.com>
2026-03-02 13:50:32 -0300

    Update PiDin.tsx

--- src/pages/Butique/PiDin.tsx
+++ src/pages/Butique/PiDin.tsx
@@ -4,7 +4,8 @@ export default function PiDin() {
   return (
     <ProductTemplate 
       category="Liivro" 
-      name="PiDin" 
+      name="PiDin"
+      image="/pidin.jpg"
       description="O dinheiro é a medida da sua liberdade. Se você não o controla, ele controla você. O mundo grita sobre “ficar rico”. O PiDin sussurra sobre ser livre." 
       price="R$ 88" 
       link="https://pay.kiwify.com.br/i9cgoaj" 
