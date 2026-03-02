92d0741b1f31b3c782418eb9ba0999d68596cbd4
Din <dinnuness@gmail.com>
2026-03-02 13:05:18 -0300

    Update PiSofia.tsx

--- src/pages/Butique/PiSofia.tsx
+++ src/pages/Butique/PiSofia.tsx
@@ -1,13 +1,13 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
-export default function PiSofia() {
+export default function Pistilo() {
   return (
     <ProductTemplate 
       category="Liivro" 
-      name="PiSofia" 
-      description="Você é o autor da sua história? Onde termina a sua natureza e começa a sua máscara? PiSofia." 
-      price="R$ 118" 
-      link="https://pay.kiwify.com.br/CqPNhIl" 
+      name="Pistilo" 
+      description="O estilo é o que resta quando a moda passa. É a sua primeira palavra. Dita antes mesmo de você abrir a boca. Pistilo." 
+      price="R$ 78" 
+      link="https://pay.kiwify.com.br/qh7pDSq" 
     />
   );
 }
