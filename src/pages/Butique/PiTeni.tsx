9082ca540c4b1ce186bf3239be97128a5ca21956
Din <dinnuness@gmail.com>
2026-03-02 13:39:00 -0300

    Update PiTeni.tsx

--- src/pages/Butique/PiTeni.tsx
+++ src/pages/Butique/PiTeni.tsx
@@ -4,7 +4,8 @@ export default function PiTeni() {
   return (
     <ProductTemplate 
       category="Liivro" 
-      name="PiTeni" 
+      name="PiTeni"
+      image="/piteni.jpg"
       description="A maioria usa o entretenimento para fugir da realidade. O PiTeni é para quem quer expandi-la." 
       price="R$ 88" 
       link="https://pay.kiwify.com.br/LPKOeCU" 
