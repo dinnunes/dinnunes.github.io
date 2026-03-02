3eba7da4bebb1c572a3f31389bbbe00cb3f88ad2
Din <dinnuness@gmail.com>
2026-03-02 10:46:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/p/', // Isso garante que os links funcionem no GitHub Pages
+    base: '/', // Isso garante que os links funcionem no GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
