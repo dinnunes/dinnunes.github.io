b21e1338bd04e6a0e63733cb65932e525f078b7a
Din <dinnuness@gmail.com>
2026-03-02 14:45:52 -0300

    Update Mim.tsx

--- src/pages/Sobre/Mim.tsx
+++ src/pages/Sobre/Mim.tsx
@@ -4,12 +4,23 @@ export default function Mim() {
   return (
     <PageTemplate title="Sobre Mim">
       <p>
-        A Pitore nasceu de uma paixão profunda pelo design minimalista e pela busca incessante pela perfeição. 
-        Acreditamos que a verdadeira elegância reside na simplicidade e na atenção meticulosa aos detalhes.
+        DIN
+        Artista
       </p>
       <p>
-        Cada peça é concebida com um propósito claro: elevar o cotidiano através de formas puras e materiais excepcionais. 
-        Nossa jornada é guiada pela convicção de que menos é, invariavelmente, mais.
+        Eu, Marcos, mais conhecido por “Din”, estou entre o Rio Grande do Sul, Santa Catarina e São Paulo, tenho 21 anos – 2022, Solteiro? Só que não, Time? Corinthians, Comida? Pizza, Signo? Áries, Cor? Rosa, Sonho? Empresa que tenho em mente. Sou apaixonado por teatro (Cinéfilo) e uma pessoa que aprecia muito a arte.
+
+        Quando mais novo eu queria ser Jogador de Futebol, nossa, eu gostava demais, tinha talento, mas… não aconteceu, eu acreditava que não conseguiria ser jogador no CT onde treinava, achava que ali não tinha potencial e tal, e acabou meio que de fato eu não me tornei jogador, eu fiquei um longo tempo sem treinar, até para de vez logo em seguida. Hoje de vez em quando eu jogo com os amigos por aí.
+
+        Mais tarde comecei a obrar em uma empresa aqui na cidade, 1° emprego. Nessa época eu "estudava" também, reprovei de ano duas vezes aliás, e indo pra terceira já, até por isso com o tempo fui parando de ir pra escola, até para de vez no 1° Ano do Médio, sei lá, apesar de não saber o que eu queria da vida direito, sabia que ali não ia me levar pra onde eu achava que queria chegar de uma certa maneira, então; ufa.
+
+        Eu sempre fui um cara mais esperto sabe, deve ser porque era mais tímido quando mais novo, mas mesmo assim isso não me impediu de fazer umas dívidas, não foi uma coisa enorme, (Uns R$3.500,00) mais foi o suficiente pra querer saber “Como sair duma situação chata dessas?” E ai em um belíssimo dia buscando... me deu vontade de comprar o livro “Pai Rico, Pai Pobre”, um livro de Robert Kiyosaki e Sharon L. Lechter.
+
+        Minha vida nunca mais foi a mesma depois do livro, (No bom sentido e no mau também) mas sem entrar em muitos detalhes, eu fui ajustando as finanças até chegar ao ponto de querer mexer com investimentos e sair da empresa onde já trabalhava e vive disso, uau, é isso aí. Então comecei a investir, “porém, porém” não demorou muito até eu perceber que investir é muito mais vantajoso quando se é rico, bom, sendo assim, eu sai da empresa, peguei o dinheiro e fui pro plano B apostando no tempo de 3 anos no máximo pra criar um negócio.
+
+        Era óbvio que conforme eu criava o negócio, mais eu pensava que deveria ter começado a empreender primeiro ao invés de investir, incrível, eu acho que eu só comecei com investimentos por ser mais fácil, isso seduz! E aquilo que antes era só pra ganhar mais dinheiro (Negócio), passou a ser uma coisa em que eu estava gostando mesmo sem tem gerado nada ainda, a não ser conhecimento, enfim, por isso desde então venho obrando nisso e outras vontades mais a longo prazo.
+
+        Irônico é que eu tinha feito um curso de “Administração” presencialmente lá atrás, e mesmo não gostando tanto no começo (Eu odiava), no final achei interessante, era um curso longo e completo, mas que eu deveria ter valorizado mais, afinal de contas hoje eu estou envolvido com, né. Tudo bem, é isso, um pouco sobre mim.
       </p>
     </PageTemplate>
   );
