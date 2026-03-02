175b64d2f42d4f7a35750a0bc9b0c72524497b3a
Din <dinnuness@gmail.com>
2026-03-02 13:00:44 -0300

    Update PiSau.tsx

--- src/pages/Butique/PiSau.tsx
+++ src/pages/Butique/PiSau.tsx
@@ -1,5 +1,13 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
 export default function PiSau() {
-  return <ProductTemplate name="PiSau" description="Conforto atemporal. Uma peça essencial que transcende as estações." />;
+  return (
+    <ProductTemplate 
+      category="Liivro" 
+      name="PiSau" 
+      description="Existe um estado de existência que você ainda não acessou. Uma harmonia que se encontra no PiSau." 
+      price="R$ 108" 
+      link="https://pay.kiwify.com.br/giILQS6" 
+    />
+  );
 }
