81a0b590b098071cd3a14aefe9854e350b9747d8
Din <dinnuness@gmail.com>
2026-03-02 12:23:34 -0300

    Update PiTeni.tsx

--- src/pages/Butique/PiTeni.tsx
+++ src/pages/Butique/PiTeni.tsx
@@ -1,5 +1,13 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
 export default function PiTeni() {
-  return <ProductTemplate category="Liivro" name="PiTeni" description="A elegância em movimento. Feito com materiais de alta performance e design minimalista." price="R$ 88" link="https://pay.kiwify.com.br/LPKOeCU" />;
+  return (
+    <ProductTemplate 
+      category="Liivro" 
+      name="PiTeni" 
+      description="Entretenimento, por gentileza." 
+      price="R$ 88" 
+      link="https://pay.kiwify.com.br/LPKOeCU" 
+    />
+  );
 }
