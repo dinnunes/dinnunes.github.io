5af5389bfef51114739d99b7535784b0ca969062
Din <dinnuness@gmail.com>
2026-03-05 19:00:44 -0300

    Update PiTeni.tsx

--- src/pages/Butique/PiTeni.tsx
+++ src/pages/Butique/PiTeni.tsx
@@ -1,14 +1,22 @@
 import ProductTemplate from '../../components/ProductTemplate';
+import FAQ from '../../components/FAQ'; // Importando o nosso novo componente
 
 export default function PiTeni() {
   return (
-    <ProductTemplate 
-      category="Liivro" 
-      name="PiTeni"
-      image="/piteni.jpg"
-      description="A maioria usa o entretenimento para fugir da realidade. O PiTeni é para quem quer expandi-la." 
-      price="R$ 88" 
-      link="https://pay.kiwify.com.br/LPKOeCU" 
-    />
+    <>
+      <ProductTemplate 
+        category="Liivro" 
+        name="PiTeni"
+        image="/piteni.jpg"
+        description="A maioria usa o entretenimento para fugir da realidade. O PiTeni é para quem quer expandi-la." 
+        price="R$ 88" 
+        link="https://pay.kiwify.com.br/LPKOeCU" 
+      />
+      
+      {/* O FAQ vai aparecer logo abaixo da estrutura do produto */}
+      <div className="pb-20">
+        <FAQ />
+      </div>
+    </>
   );
 }
