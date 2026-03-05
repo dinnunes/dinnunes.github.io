535b9cc775f7515884ea83c06d1cec608f848947
Din <dinnuness@gmail.com>
2026-03-05 19:21:02 -0300

    Update Pistilo.tsx

--- src/pages/Butique/Pistilo.tsx
+++ src/pages/Butique/Pistilo.tsx
@@ -1,14 +1,20 @@
 import ProductTemplate from '../../components/ProductTemplate';
+import FAQ from '../../components/FAQ';
 
 export default function Pistilo() {
   return (
-    <ProductTemplate 
-      category="Liivro" 
-      name="Pistilo"
-      image="/pistilo.jpg"
-      description="O estilo é o que resta quando a moda passa. É a sua primeira palavra. Dita antes mesmo de você abrir a boca. Pistilo." 
-      price="R$ 78" 
-      link="https://pay.kiwify.com.br/qh7pDSq" 
-    />
+    <>
+      <ProductTemplate 
+        category="Liivro" 
+        name="Pistilo"
+        image="/pistilo.jpg"
+        description="O estilo é o que resta quando a moda passa. É a sua primeira palavra. Dita antes mesmo de você abrir a boca. Pistilo." 
+        price="R$ 78" 
+        link="https://pay.kiwify.com.br/qh7pDSq" 
+      />
+      <div className="pb-20">
+        <FAQ />
+      </div>
+    </>
   );
 }
