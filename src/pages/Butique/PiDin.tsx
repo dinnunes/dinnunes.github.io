8c8e0a1e7e2c883e6d1d76b7db9d051ebc80977b
Din <dinnuness@gmail.com>
2026-03-05 19:12:20 -0300

    Update PiDin.tsx

--- src/pages/Butique/PiDin.tsx
+++ src/pages/Butique/PiDin.tsx
@@ -1,14 +1,20 @@
 import ProductTemplate from '../../components/ProductTemplate';
+import FAQ from '../../components/FAQ';
 
 export default function PiDin() {
   return (
-    <ProductTemplate 
-      category="Liivro" 
-      name="PiDin"
-      image="/pidin.jpg"
-      description="O dinheiro é a medida da sua liberdade. Se você não o controla, ele controla você. O mundo grita sobre “ficar rico”. O PiDin sussurra sobre ser livre." 
-      price="R$ 88" 
-      link="https://pay.kiwify.com.br/i9cgoaj" 
-    />
+    <>
+      <ProductTemplate 
+        category="Liivro" 
+        name="PiDin"
+        image="/pidin.jpg"
+        description="O dinheiro é a medida da sua liberdade. Se você não o controla, ele controla você. O mundo grita sobre “ficar rico”. O PiDin sussurra sobre ser livre." 
+        price="R$ 88" 
+        link="https://pay.kiwify.com.br/i9cgoaj" 
+      />
+      <div className="pb-20">
+        <FAQ />
+      </div>
+    </>
   );
 }
